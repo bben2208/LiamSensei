3599032a8de3c78daa8fcbdf1c6ea1df253ae247
bben2208 <bogbeni431@gmail.com>
2024-09-09 16:30:31 +0100

    adding inputTextButton

--- main.js
+++ main.js
@@ -6,3 +6,23 @@
     const currentTime = new Date()
     alert(currentTime)
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const textButton = document.getElementById('textButton');
+    const longTextArea = document.getElementById('longTextArea');
+    const noteSection = document.getElementById('noteSection');
+  
+    textButton.addEventListener('click', function () {
+     
+      const textValue = longTextArea.value;
+  
+      const newParagraph = document.createElement('p');
+  
+      newParagraph.textContent = textValue;
+  
+      noteSection.appendChild(newParagraph);
+  
+      longTextArea.value = '';
+    });
+  });
+  
