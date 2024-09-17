4ff0a1f04f49c667573c43c66cd4a78cca3bc8f9
bben2208 <bogbeni431@gmail.com>
2024-09-17 13:52:36 +0100

    moved to under function

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
  console.log('Hello World')
 
- const alertCurrentTime = document.getElementById('alertCurrentTime')
  
- alertCurrentTime.addEventListener('click',function(){
-    const currentTime = new Date()
-    alert(currentTime)
-})
 
 document.addEventListener('DOMContentLoaded', function () {
     const textButton = document.getElementById('textButton');
     const  textInput = document.getElementById('textInput');
     const textList = document.getElementById('textList');
+
+    const alertCurrentTime = document.getElementById('alertCurrentTime')
+ 
+ alertCurrentTime.addEventListener('click',function(){
+    const currentTime = new Date()
+    alert(currentTime)
+})
   
     textButton.addEventListener('click', function () {
      
