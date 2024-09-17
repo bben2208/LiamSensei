4e9aa06617220f2d00591098853fc57e11be322f
bben2208 <bogbeni431@gmail.com>
2024-09-17 16:58:48 +0100

    code is working

--- main.js
+++ main.js
@@ -3,19 +3,18 @@
  
 
 document.addEventListener('DOMContentLoaded',function () {
-    const textButton = document.getElementById('textButton');
+    const textButton = document.getElementById('inputButton');
     const textInput = document.getElementById('textInput');
     const textList = document.getElementById('textList');
 
-    const alertCurrentTime = document.getElementById('alertCurrentTime')
+    // const alertCurrentTime = document.getElementById('alertCurrentTime')
  
- alertCurrentTime.addEventListener('click',function(){
-    const currentTime = new Date()
-    alert(currentTime)
-})
+ 
+
+
   
-    textButton.addEventListener('click',function () {
-     
+    textButton.addEventListener('click',function(){
+        console.log('Button clicked');
       const textValue = textInput.value;
   
       const  newList = document.createElement('li');
@@ -28,3 +27,7 @@ document.addEventListener('DOMContentLoaded',function () {
     });
   });
   
+//   alertCurrentTime.addEventListener('click',function(){
+//     const currentTime = new Date()
+//     alert(currentTime)
+//  })
